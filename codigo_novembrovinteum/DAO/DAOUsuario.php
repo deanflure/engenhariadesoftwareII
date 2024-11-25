baf20b504dfc85223ff4a19490e59b1e65d5c5f1
Rhaynnara <rhaynnarafernandespereira@gmail.com>
2024-11-25 19:26:04 -0300

    new

--- codigo_novembrovinteum/DAO/DAOUsuario.php
+++ codigo_novembrovinteum/DAO/DAOUsuario.php
@@ -10,16 +10,9 @@ require($root . 'codigo_novembrovinteum/models/Usuario.php');
 
 use models\Usuario;
 
-/**
- * Esta classe é reponsável por fazer a comunicação com o banco de dados,
- * provendo as funções de logar e incluir um novo usuário
- *
- * @author rhaynnara
- *
- */
 class DAOUsuario {
     /**
-     * Faz o login no sisema validando os dados fornecidos pelo usuário
+     * Faz o login no sistema validando os dados fornecidos pelo usuário
      * @param string $login Login do usuário
      * @param string $senha Senha do usuário
      * @return Usuario
@@ -33,8 +26,7 @@ class DAOUsuario {
 
         $usuario = new Usuario();
 
-        $sql = $conexaoDB->prepare("SELECT login, nome, email, celular FROM usuario
-                                    WHERE login = ? AND senha = ?");
+        $sql = $conexaoDB->prepare("SELECT login, nome, email, celular FROM usuario WHERE login = ? AND senha = ?");
         $sql->bind_param("ss", $login, $senha);
         $sql->execute();
 
@@ -56,8 +48,8 @@ class DAOUsuario {
      * @param string $nome Nome do usuário
      * @param string $email Email do usuário
      * @param string $login Login do usuário
-     * @param string $senha senha do usuário
-     * @return TRUE|Exception TRUE para inclusão bem sucedida ou Exception para inclusão mal sucedida
+     * @param string $senha Senha do usuário
+     * @return TRUE|Exception TRUE para inclusão bem-sucedida ou Exception para falha
      */
     public function incluirUsuario($nome, $email, $login, $senha) {
         try {
@@ -77,6 +69,87 @@ class DAOUsuario {
         }
 
         $sqlInsert->close();
+        $conexaoDB->close();
+        return $retorno;
+    }
+
+    /**
+     * Lista todos os usuários do banco de dados
+     * @return array Lista de usuários
+     */
+    public function listarUsuarios() {
+        try {
+            $conexaoDB = $this->conectarBanco();
+        } catch (\Exception $e) {
+            die($e->getMessage());
+        }
+
+        $sql = "SELECT id, nome, email, login FROM usuario";
+        $resultado = $conexaoDB->query($sql);
+
+        $usuarios = [];
+        if ($resultado->num_rows > 0) {
+            while ($linha = $resultado->fetch_assoc()) {
+                $usuarios[] = $linha;
+            }
+        }
+
+        $conexaoDB->close();
+        return $usuarios;
+    }
+
+    /**
+     * Exclui um usuário pelo ID
+     * @param int $id ID do usuário a ser excluído
+     * @return bool TRUE em caso de sucesso, ou Exception em caso de falha
+     */
+    public function excluirUsuario($id) {
+        try {
+            $conexaoDB = $this->conectarBanco();
+        } catch (\Exception $e) {
+            die($e->getMessage());
+        }
+
+        $sqlDelete = $conexaoDB->prepare("DELETE FROM usuario WHERE id = ?");
+        $sqlDelete->bind_param("i", $id);
+        $sqlDelete->execute();
+
+        if ($sqlDelete->affected_rows > 0) {
+            $retorno = TRUE;
+        } else {
+            throw new \Exception("Erro ao excluir usuário. ID não encontrado.");
+        }
+
+        $sqlDelete->close();
+        $conexaoDB->close();
+        return $retorno;
+    }
+
+    /**
+     * Atualiza os dados de um usuário
+     * @param int $id ID do usuário
+     * @param string $nome Nome do usuário
+     * @param string $email Email do usuário
+     * @return bool TRUE em caso de sucesso, ou Exception em caso de falha
+     */
+    public function atualizarUsuario($id, $nome, $email) {
+        try {
+            $conexaoDB = $this->conectarBanco();
+        } catch (\Exception $e) {
+            die($e->getMessage());
+        }
+
+        $sqlUpdate = $conexaoDB->prepare("UPDATE usuario SET nome = ?, email = ? WHERE id = ?");
+        $sqlUpdate->bind_param("ssi", $nome, $email, $id);
+        $sqlUpdate->execute();
+
+        if ($sqlUpdate->affected_rows > 0) {
+            $retorno = TRUE;
+        } else {
+            throw new \Exception("Erro ao atualizar usuário. Verifique os dados informados.");
+        }
+
+        $sqlUpdate->close();
         $conexaoDB->close();
         return $retorno;
     }
@@ -103,3 +176,4 @@ class DAOUsuario {
     }
 }
 ?>
+
